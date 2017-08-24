417d84e62e9867ae5d8696df954a48065b24b0cc
kkras <kkrastev@live.com>
2017-08-25 02:03:28 +0300

    Added searchable configuration. Added Search Widget to App Toolbar. Added lonch and search icons.

--- app/src/main/java/com/mosy/kalin/mosy/VenuesActivity.java
+++ app/src/main/java/com/mosy/kalin/mosy/VenuesActivity.java
@@ -1,51 +1,57 @@
 package com.mosy.kalin.mosy;
 
-import android.Manifest;
+import android.app.SearchManager;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.content.PermissionChecker;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.mosy.kalin.mosy.Adapters.VenuesAdapter;
 import com.mosy.kalin.mosy.DTOs.Venue;
 import com.mosy.kalin.mosy.Services.LocationResolver;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ItemClick;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.OptionsMenuItem;
+import org.androidannotations.annotations.SystemService;
 import org.androidannotations.annotations.ViewById;
 
+
 @EActivity(R.layout.activity_venues)
+@OptionsMenu(R.menu.search_menu)
 public class VenuesActivity
         extends AppCompatActivity
 {
     private LocationResolver mLocationResolver;
-
-    @ViewById(resName = "venues_lvVenues")
-    ListView Venues;
+    SearchView searchView;
 
     @Bean
     VenuesAdapter adapter;
+    @SystemService
+    SearchManager searchManager;
+    @ViewById(R.id.search_toolbar)
+    Toolbar toolbar;
+    @ViewById(resName = "venues_lvVenues")
+    ListView Venues;
 
     @AfterViews
     void bindAdapter() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         final SwipeRefreshLayout swipeContainer = (SwipeRefreshLayout)findViewById(R.id.venues_lSwipeContainer);
         adapter.setSwipeRefreshLayout(swipeContainer);
 
@@ -65,8 +71,23 @@ public class VenuesActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venues);
 
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            performSearch(query);
+        }
         mLocationResolver = new LocationResolver(this);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+//      searchView.setFocusable(true); searchView.setIconified(false); searchView.requestFocusFromTouch();
+        return true;
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -89,8 +110,8 @@ public class VenuesActivity
         mLocationResolver.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
-    @ItemClick
-    void venues_lvVenuesItemClicked(Venue venue) {
+    @ItemClick(resName = "venues_lvVenues")
+    void openMenu(Venue venue) {
         Intent intent = new Intent(VenuesActivity.this, VenueActivity_.class);
         venue.OutdoorImage = null; // Don't need these one in the Venue page. If needed should implement Serializable or Parcelable
         venue.Location = null;
@@ -107,5 +128,8 @@ public class VenuesActivity
         });
     }
 
+    private void performSearch(String query) {
+        Toast.makeText(getApplicationContext(), "Searching for " + query + " .." , Toast.LENGTH_LONG).show();
+    }
 
 }
