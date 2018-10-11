baf226b3229889ba0086b097ef84eddbdbd0e8b7
Kamen <kamen_prodanov@yahoo.com>
2018-10-11 13:19:59 +0300

    Added a Profile Icon; Implemented Profile Activity and NavigateToProfileActivity.

---
+++ app/src/main/java/com/mosy/kalin/mosy/UserProfileActivity.java
@@ -0,0 +1,7 @@
+package com.mosy.kalin.mosy;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_userprofile)
+public class UserProfileActivity extends BaseActivity {
+}
