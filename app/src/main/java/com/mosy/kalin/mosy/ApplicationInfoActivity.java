7e91a9329d6bd2a9ca536aa7808f93d9fe9cf663
Kamen <kamen_prodanov@yahoo.com>
2018-11-27 12:50:44 +0200

    Working to display the Version in a Text view.

--- app/src/main/java/com/mosy/kalin/mosy/ApplicationInfoActivity.java
+++ app/src/main/java/com/mosy/kalin/mosy/ApplicationInfoActivity.java
@@ -1,9 +1,32 @@
 package com.mosy.kalin.mosy;
 
+import android.widget.TextView;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_app_info)
 public class ApplicationInfoActivity extends BaseActivity {
 
+    @ViewById(R.id.appinfo_appversion)
+    TextView AppInfo;
+
+    int versionCode = BuildConfig.VERSION_CODE;
+    String versionName = BuildConfig.VERSION_NAME;
+
+    @AfterViews
+    void afterViews() {
+
+
+
+
+
+        this.AppInfo.setText(this.versionName);
+    }
+
+
+
 
 }
+
